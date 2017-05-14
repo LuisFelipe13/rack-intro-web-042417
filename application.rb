c7d8d99fa01be5c31b8745389ae5b43d77cebe73
Luis del Cueto <luisdelcueto@live.com>
2017-05-14 15:27:57 -0400

    Done.

--- application.rb
+++ application.rb
@@ -2,9 +2,8 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is Luis Felipe del Cueto"
     resp.finish
   end
 
 end
-
